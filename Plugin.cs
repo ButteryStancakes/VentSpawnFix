940c50003af264c2bffc77474d1d8bb608f74a4a
ButteryStancakes <jlj3322@gmail.com>
2024-06-30 19:28:38 -0700

    Support for spawnInGroupsOf

--- Plugin.cs
+++ Plugin.cs
@@ -2,6 +2,7 @@
 using BepInEx.Logging;
 using HarmonyLib;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace VentSpawnFix
@@ -9,7 +10,7 @@ namespace VentSpawnFix
     [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        const string PLUGIN_GUID = "butterystancakes.lethalcompany.ventspawnfix", PLUGIN_NAME = "Vent Spawn Fix", PLUGIN_VERSION = "1.0.1";
+        const string PLUGIN_GUID = "butterystancakes.lethalcompany.ventspawnfix", PLUGIN_NAME = "Vent Spawn Fix", PLUGIN_VERSION = "1.1.0";
         internal static new ManualLogSource Logger;
 
         void Awake()
@@ -33,14 +34,11 @@ namespace VentSpawnFix
             {
                 Plugin.Logger.LogInfo($"A new enemy tried to occupy vent with {vent.enemyType.name} already inside");
 
-                List<EnemyVent> vents = new();
-                foreach (EnemyVent enemyVent in __instance.allEnemyVents)
-                    if (!enemyVent.occupied)
-                        vents.Add(enemyVent);
+                List<EnemyVent> vents = __instance.allEnemyVents.Where(enemyVent => !enemyVent.occupied).ToList();
                 
                 if (vents.Count < 1)
                 {
-                    Plugin.Logger.LogInfo("All vents on the map are occupied");
+                    Plugin.Logger.LogInfo("Enemy spawn cancelled because all vents on the map are occupied");
                     __result = false;
                     return false;
                 }
@@ -50,6 +48,78 @@ namespace VentSpawnFix
             }
 
             return true;
+        }
+
+        [HarmonyPatch(typeof(RoundManager), "AssignRandomEnemyToVent")]
+        [HarmonyPostfix]
+        static void RoundManagerPostAssignRandomEnemyToVent(RoundManager __instance, EnemyVent vent, int ___currentHour)
+        {
+            EnemyType enemy = vent.enemyType;
+            if (enemy.spawnInGroupsOf > 1)
+            {
+                Plugin.Logger.LogInfo($"Enemy \"{enemy.name}\" spawned in vent, requesting group of {enemy.spawnInGroupsOf}");
+
+                int enemyIndex = -1;
+                for (int i = 0; i < __instance.currentLevel.Enemies.Count; i++)
+                {
+                    if (__instance.currentLevel.Enemies[i].enemyType == enemy)
+                    {
+                        enemyIndex = i;
+                        break;
+                    }
+                }
+
+                if (enemyIndex < 0)
+                {
+                    Plugin.Logger.LogWarning($"Tried to spawn additional \"{enemy.name}\", but they do not exist in the current level's spawn pool");
+                    return;
+                }
+
+                int spawnsLeft = enemy.spawnInGroupsOf;
+                List<EnemyVent> vents = __instance.allEnemyVents.Where(enemyVent => !enemyVent.occupied).ToList();
+
+                while (spawnsLeft > 0)
+                {
+                    if (vents.Count <= 0)
+                    {
+                        Plugin.Logger.LogInfo($"Can't spawn additional \"{enemy.name}\" (all vents are occupied)");
+                        return;
+                    }
+
+                    if (enemy.numberSpawned >= enemy.MaxCount)
+                    {
+                        Plugin.Logger.LogInfo($"Can't spawn additional \"{enemy.name}\" ({enemy.MaxCount} have already spawned)");
+                        return;
+                    }
+
+                    if (enemy.PowerLevel > __instance.currentLevel.maxEnemyPowerCount - __instance.currentEnemyPower)
+                    {
+                        Plugin.Logger.LogInfo($"Can't spawn additional \"{enemy.name}\" ({__instance.currentEnemyPower} + {enemy.PowerLevel} would exceed max power level of {__instance.currentLevel.maxEnemyPowerCount})");
+                        return;
+                    }
+
+                    int time = (int)(__instance.timeScript.lengthOfHours * ___currentHour) + __instance.AnomalyRandom.Next(10, (int)__instance.timeScript.lengthOfHours * __instance.hourTimeBetweenEnemySpawnBatches);
+                    EnemyVent vent2 = vents[__instance.AnomalyRandom.Next(0, vents.Count)];
+
+                    __instance.currentEnemyPower += enemy.PowerLevel;
+                    vent2.enemyType = enemy;
+                    vent2.enemyTypeIndex = enemyIndex;
+                    vent2.occupied = true;
+                    vent2.spawnTime = time;
+                    if (__instance.timeScript.hour - ___currentHour <= 0)
+                        vent2.SyncVentSpawnTimeClientRpc(time, enemyIndex);
+                    enemy.numberSpawned++;
+
+                    spawnsLeft--;
+                    vents.Remove(vent2);
+                    __instance.enemySpawnTimes.Add(time);
+
+                    Plugin.Logger.LogInfo($"Spawning additional \"{enemy.name}\" in vent");
+                }
+
+                if (spawnsLeft < enemy.spawnInGroupsOf)
+                    __instance.enemySpawnTimes.Sort();
+            }
         }
     }
 }
