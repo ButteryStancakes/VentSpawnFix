57ce6790232ca89dd3b454b2b9aa0d7bdd384280
ButteryStancakes <jlj3322@gmail.com>
2024-10-21 12:52:18 -0500

    Prevent "ghost spawns" after grouping behavior

--- Plugin.cs
+++ Plugin.cs
@@ -11,7 +11,7 @@ namespace VentSpawnFix
     //[BepInDependency("Dev1A3.LethalFixes", BepInDependency.DependencyFlags.SoftDependency)]
     public class Plugin : BaseUnityPlugin
     {
-        const string PLUGIN_GUID = "butterystancakes.lethalcompany.ventspawnfix", PLUGIN_NAME = "Vent Spawn Fix", PLUGIN_VERSION = "1.2.1";
+        const string PLUGIN_GUID = "butterystancakes.lethalcompany.ventspawnfix", PLUGIN_NAME = "Vent Spawn Fix", PLUGIN_VERSION = "1.2.2";
         internal static new ManualLogSource Logger;
 
         void Awake()
@@ -35,6 +35,31 @@ namespace VentSpawnFix
     class VentSpawnFixPatches
     {
         [HarmonyPatch(typeof(RoundManager), "AssignRandomEnemyToVent")]
+        [HarmonyPrefix]
+        static bool RoundManagerPreAssignRandomEnemyToVent(RoundManager __instance, ref EnemyVent vent, ref bool __result)
+        {
+            // this is needed, actually, to avoid group spawns being overridden
+            if (vent.occupied)
+            {
+                Plugin.Logger.LogDebug($"A new enemy tried to occupy vent with \"{vent.enemyType.enemyName}\" already inside");
+
+                List<EnemyVent> vents = __instance.allEnemyVents.Where(enemyVent => !enemyVent.occupied).ToList();
+
+                if (vents.Count < 1)
+                {
+                    Plugin.Logger.LogDebug("Enemy spawn cancelled because all vents on the map are occupied");
+                    __result = false;
+                    return false;
+                }
+
+                vent = vents[__instance.AnomalyRandom.Next(0, vents.Count)];
+                Plugin.Logger.LogDebug("Enemy successfully reassigned to another empty vent");
+            }
+
+            return true;
+        }
+
+        [HarmonyPatch(typeof(RoundManager), "AssignRandomEnemyToVent")]
         [HarmonyPostfix]
         static void RoundManagerPostAssignRandomEnemyToVent(RoundManager __instance, EnemyVent vent, bool __result, int ___currentHour)
         {
@@ -46,6 +71,12 @@ namespace VentSpawnFix
             if (enemy == null)
             {
                 Plugin.Logger.LogWarning("AssignRandomEnemyToVent completed without assigning an enemy to the vent. This shouldn't happen");
+                return;
+            }
+
+            if (vent.enemyTypeIndex < 0 || vent.enemyTypeIndex > __instance.currentLevel.Enemies.Count || !__instance.currentLevel.Enemies.Any(spawnableEnemyWithRarity => spawnableEnemyWithRarity.enemyType == enemy))
+            {
+                Plugin.Logger.LogWarning("AssignRandomEnemyToVent assigned an enemy with an invalid index. This shouldn't happen");
                 return;
             }
 
@@ -64,11 +95,11 @@ namespace VentSpawnFix
                         return;
                     }
 
-                    /*if (enemy.numberSpawned >= enemy.MaxCount)
+                    if (enemy.numberSpawned >= enemy.MaxCount)
                     {
-                        Plugin.Logger.LogInfo($"Can't spawn additional \"{enemy.enemyName}\" ({enemy.MaxCount} have already spawned)");
+                        Plugin.Logger.LogDebug($"Can't spawn additional \"{enemy.enemyName}\" ({enemy.MaxCount} have already spawned)");
                         return;
-                    }*/
+                    }
 
                     if (enemy.PowerLevel > __instance.currentMaxInsidePower - __instance.currentEnemyPower)
                     {
